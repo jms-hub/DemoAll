68f4c52eec6e28bc26cd8f581b1b62bb0f01c1e3
贾敏升 <1263165880@qq.com>
2020-02-12 12:56:51 +0800

    sssddd

---
+++ src/星期三/test.java
@@ -0,0 +1,8 @@
+package 星期三;
+
+public class test {
+    public static void main(String[] args) {
+        NzThread nzThread = new NzThread();// 创建自定义的一个线程
+        nzThread.start();//  调用start方法，让线程就绪
+    }
+}
